422c3474824a4e23fb05e983a8b52d4a7136eb59
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2018-05-13 15:11:40 +0200

    add simple controller

--- bar-service/src/main/java/com/example/barservice/BarServiceApplication.java
+++ bar-service/src/main/java/com/example/barservice/BarServiceApplication.java
@@ -2,12 +2,22 @@ package com.example.barservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BarServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BarServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BarServiceApplication.class, args);
+    }
+}
+
+@RestController
+class BarController {
+
+    @GetMapping
+    String bar() {
+        return "bar";
+    }
 }
--- foo-service/src/main/java/com/example/fooservice/FooServiceApplication.java
+++ foo-service/src/main/java/com/example/fooservice/FooServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.fooservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class FooServiceApplication {
@@ -10,4 +12,14 @@ public class FooServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FooServiceApplication.class, args);
 	}
+}
+
+@RestController
+class FooController {
+
+	@GetMapping
+	public String foo() {
+		return "foo";
+	}
+
 }
