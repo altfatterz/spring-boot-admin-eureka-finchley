cabf9b3d9895072f7d71eb3d8690d107b0921616
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2018-05-13 16:15:59 +0200

    call from foo-service to the bar-service

--- foo-service/src/main/java/com/example/fooservice/FooServiceApplication.java
+++ foo-service/src/main/java/com/example/fooservice/FooServiceApplication.java
@@ -2,24 +2,61 @@ package com.example.fooservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient // need to reach the bar-service, otherwise "java.lang.IllegalStateException: No instances available for bar-service"
 public class FooServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FooServiceApplication.class, args);
-	}
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooServiceApplication.class, args);
+    }
 }
 
 @RestController
 class FooController {
 
-	@GetMapping
-	public String foo() {
-		return "foo";
-	}
+    private final BarClient barClient;
 
+    public FooController(BarClient barClient) {
+        this.barClient = barClient;
+    }
+
+    @GetMapping
+    public String foo() {
+        return "foo";
+    }
+
+    @GetMapping("/foobar")
+    public String fooBar() {
+        return "foo" + barClient.getBar();
+    }
+
+}
+
+@Component
+class BarClient {
+
+    private final RestTemplate restTemplate;
+
+    public BarClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getBar() {
+        return restTemplate.getForObject("http://bar-service", String.class);
+    }
 }
