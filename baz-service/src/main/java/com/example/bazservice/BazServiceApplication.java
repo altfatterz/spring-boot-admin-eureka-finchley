cbe0668fc400443e1c2a8725e674c6c4e0d2b095
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2018-05-14 17:40:22 +0200

    add security to foo and bar service and introduce baz-service which uses spring boot admin client api instead of eureka discovery

---
+++ baz-service/src/main/java/com/example/bazservice/BazServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.bazservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazServiceApplication.class, args);
+	}
+}
